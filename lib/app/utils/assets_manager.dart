407aeb29bbdad0c282bc671f7135be095f3c052b
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-17 23:43:55 +0200

    add needed assets and those manager

---
+++ lib/app/utils/assets_manager.dart
@@ -0,0 +1,27 @@
+const String imagePath = "assets/images";
+const String iconPath = "assets/icons";
+
+class ImageAssets {
+  static const String logo = "$imagePath/logo.svg";
+  static const String allDone = "$imagePath/all_done.svg";
+}
+
+class IconAssets {
+  static const String facebook = "$iconPath/facebook.svg";
+  static const String twitter = "$iconPath/twitter.svg";
+  static const String google = "$iconPath/google.svg";
+  static const String menubar = "$iconPath/menubar.svg";
+  static const String mark = "$iconPath/mark.svg";
+  static const String alarm = "$iconPath/alarm.svg";
+  static const String add = "$iconPath/add.svg";
+  static const String important = "$iconPath/important.svg";
+  static const String done = "$iconPath/done.svg";
+  static const String later = "$iconPath/later.svg";
+  static const String category = "$iconPath/category.svg";
+  static const String settings = "$iconPath/settings.svg";
+  static const String logout = "$iconPath/logout.svg";
+  static const String back = "$iconPath/back.svg";
+  static const String search = "$iconPath/search.svg";
+  static const String edit = "$iconPath/edit.svg";
+  static const String delete = "$iconPath/delete.svg";
+}
