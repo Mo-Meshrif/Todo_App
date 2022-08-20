f499dca43b1ecd56568337e96c688ff8f355a768
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-20 16:07:18 +0200

    add auth strings

--- lib/app/utils/strings_manager.dart
+++ lib/app/utils/strings_manager.dart
@@ -1,3 +1,11 @@
 class AppStrings{
   static const appName = "Things ToDo";
+  static const userName ='Username';
+  static const password ='Password';
+  static const login ='LOGIN';
+  static const forgetPassword='Forget Password ?';
+  static const or ='OR';
+  static const loginUsingSm ='login using social media';
+  static const noAccount ="Don't have an account ? ";
+  static const signUp='SignUp';
 }
