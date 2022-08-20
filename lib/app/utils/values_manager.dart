83cda89af457c6d5f9a1fd0654fec47f4b139f70
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-20 16:02:36 +0200

    add values manager

---
+++ lib/app/utils/values_manager.dart
@@ -0,0 +1,17 @@
+class AppSize {
+  static const double s10 = 10.0;
+  static const double s20 = 20.0;
+  static const double s36 = 36.0;
+  static const double s48 = 48.0;
+  static const double s80 = 80.0;
+  static const double s120 = 120.0;
+  static const double s144 = 144.0;
+  static const double s170 = 170.0;
+  static const double s189 = 189.0;
+  static const double s250 = 250.0;
+  static const double s295 = 295.0;
+}
+
+class AppPadding {
+  static const double p10 = 10.0;
+}
