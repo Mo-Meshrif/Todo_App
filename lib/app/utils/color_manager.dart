3ccaa193c7fa575356a78d1c28b43b98cbbc020d
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-20 16:01:03 +0200

    add border color

--- lib/app/utils/color_manager.dart
+++ lib/app/utils/color_manager.dart
@@ -4,4 +4,5 @@ class ColorManager {
   static Color primary = const Color(0xff7646FF);
   static Color swatch = const Color(0xffffffff);
   static Color background = const Color(0xfff6F6F8);
+  static Color border = const Color(0xff707070);
 }
