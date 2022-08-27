16f3018e60b5344360b223e93024e30671bdaec4
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-28 01:57:02 +0200

    add constants manager

---
+++ lib/app/utils/constants_manager.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String twitterApiKey = 'hvKiIs7bNGehX7oWTB71HWn1e';
+  static const String twitterApiSecretKey = 'sPLTtdmXy3G3rh03DHm8m1g8HTBH2ItwGP3afqTK3y7wfGtfWo';
+  static const String twitterRedirectURI = 'flutter-twitter-login://';
+  static const String googleScope='email';
+  static const String userKey='userKey';
+  static const String passLoginKey='passLoginKey';
+  static const int durationInSec=3;
+}
