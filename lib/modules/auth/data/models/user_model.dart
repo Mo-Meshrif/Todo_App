743f9402b77f01face8acfbf934c63ea9b01fe72
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-28 02:05:58 +0200

    add user model

---
+++ lib/modules/auth/data/models/user_model.dart
@@ -0,0 +1,22 @@
+import '/modules/auth/domain/entities/user.dart';
+
+class UserModel extends User {
+  const UserModel({
+    required String id,
+    required String name,
+    required String email,
+    String? pic,
+  }) : super(id: id, name: name, email: email, pic: pic);
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        pic: json['pic'],
+      );
+  toJson() => {
+        'id': id,
+        'name': name,
+        'email': email,
+        'pic': pic,
+      };
+}
