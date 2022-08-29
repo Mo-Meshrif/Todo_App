0d66b94880bc6b97c51374a67829b9af214256d3
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-29 02:23:35 +0200

    add new constants

--- lib/app/utils/constants_manager.dart
+++ lib/app/utils/constants_manager.dart
@@ -1,9 +1,15 @@
 class AppConstants {
   static const String twitterApiKey = 'hvKiIs7bNGehX7oWTB71HWn1e';
-  static const String twitterApiSecretKey = 'sPLTtdmXy3G3rh03DHm8m1g8HTBH2ItwGP3afqTK3y7wfGtfWo';
+  static const String twitterApiSecretKey =
+      'sPLTtdmXy3G3rh03DHm8m1g8HTBH2ItwGP3afqTK3y7wfGtfWo';
   static const String twitterRedirectURI = 'flutter-twitter-login://';
-  static const String googleScope='email';
-  static const String userKey='userKey';
-  static const String passLoginKey='passLoginKey';
-  static const int durationInSec=3;
+  static const String googleScope = 'email';
+  static const String usersCollection = 'Users';
+  static const String differentCredential =
+      "account-exists-with-different-credential";
+  static const String tryAgain =
+      "we can't sign into your account,try again later";
+  static const String userKey = 'userKey';
+  static const String passLoginKey = 'passLoginKey';
+  static const int durationInSec = 3;
 }
