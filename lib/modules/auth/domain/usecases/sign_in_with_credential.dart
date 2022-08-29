a1cd48d874a09b6a55cec53768e5585dcf2431ed
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-08-29 02:21:29 +0200

    add signInWithCredentialUseCase

---
+++ lib/modules/auth/domain/usecases/sign_in_with_credential.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../../../app/errors/failure.dart';
+import '../../../../app/usecase/base_use_case.dart';
+import '../entities/user.dart';
+import '../repositories/base_auth_repository.dart';
+
+class SignInWithCredentialUseCase
+    implements BaseUseCase<Either<Failure, AuthUser>, AuthCredential> {
+  final BaseAuthRepository baseAuthRepository;
+  SignInWithCredentialUseCase(this.baseAuthRepository);
+
+  @override
+  Future<Either<Failure, AuthUser>> call(AuthCredential authCredential) =>
+      baseAuthRepository.signInWithCredential(authCredential);
+}
