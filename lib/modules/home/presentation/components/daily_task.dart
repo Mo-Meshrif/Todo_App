875fa15df7a9a07adc7f88eebbb782ac5c09f85e
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-09-02 18:02:52 +0200

    init home-components

---
+++ lib/modules/home/presentation/components/daily_task.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DailyTask extends StatelessWidget {
+  const DailyTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('DAILY-TASK'),
+    );
+  }
+}
---
+++ lib/modules/home/presentation/components/monthly_task.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MonthlyTask extends StatelessWidget {
+  const MonthlyTask({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('MONTHLY-TASK'),
+    );
+  }
+}
