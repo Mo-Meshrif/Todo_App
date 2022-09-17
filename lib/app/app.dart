ae8560904dbaae91d71be78bde9d8dc70969901a
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-09-17 04:27:14 +0200

    add home bloc

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo/modules/home/presentation/controller/home_bloc.dart';
+import 'services/services_locator.dart';
 import 'utils/routes_manager.dart';
 import 'utils/strings_manager.dart';
 import '/app/utils/theme_manager.dart';
@@ -20,13 +23,17 @@ class _MyAppState extends State<MyApp> {
       designSize: const Size(1080, 1920),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, _) => MaterialApp(
-          title: AppStrings.appName,
-          theme: ThemeManager.lightTheme(),
-          onGenerateRoute: RouteGenerator.getRoute,
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
-          locale: context.locale,
-        ),
-    );
+      builder: (context, _) => MultiBlocProvider(
+            providers: [
+              BlocProvider<HomeBloc>(create: (context) => sl()),
+            ],
+            child: MaterialApp(
+              title: AppStrings.appName,
+              theme: ThemeManager.lightTheme(),
+              onGenerateRoute: RouteGenerator.getRoute,
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+            ),
+          ));
 }
