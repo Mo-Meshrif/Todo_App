6a0ce2c662b2fb9087e31912d281e2215c7aae07
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-09-17 04:28:50 +0200

    add search screen logic

--- lib/modules/home/presentation/screens/subScreens/search_screen.dart
+++ lib/modules/home/presentation/screens/subScreens/search_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+import '../../../../../app/utils/assets_manager.dart';
+import '../../../../../app/utils/strings_manager.dart';
+import '../../../domain/entities/task_to_do.dart';
+import '../../controller/home_bloc.dart';
 import '../../widgets/custom_app_bar.dart';
+import '../../widgets/custom_task_list.dart';
 import '../../widgets/custom_text_search.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -7,22 +14,63 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'Search',
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverAppBar(
-            elevation: 0,
-            automaticallyImplyLeading: false,
-            pinned: true,
-            flexibleSpace: CustomTextSearch(
-              onChanged: (val) => debugPrint(val),
+    List<TaskTodo> searchedList = [];
+    return WillPopScope(
+      onWillPop: () {
+        //use this event here to clear savedList when user exit and enter immediately
+        BlocProvider.of<HomeBloc>(context).add(ClearSearchListEvent());
+        return Future.value(true);
+      },
+      child: Scaffold(
+        appBar: const CustomAppBar(
+          title: AppStrings.search,
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              pinned: true,
+              flexibleSpace: CustomTextSearch(
+                onChanged: (val) {
+                  if (val.isNotEmpty) {
+                    BlocProvider.of<HomeBloc>(context).add(
+                      GetSearchedTasksEvent(searchedVal: val),
+                    );
+                  } else {
+                    BlocProvider.of<HomeBloc>(context)
+                        .add(ClearSearchListEvent());
+                  }
+                },
+              ),
             ),
+          ],
+          body: BlocConsumer<HomeBloc, HomeState>(
+            listener: (context, state) {
+              if (state is EditTaskLoaded) {
+                int index = searchedList
+                    .indexWhere((task) => task.id == state.task?.id);
+                if (index > -1) {
+                  searchedList[index] = state.task!;
+                }
+              } else if (state is SearchedTaskLoaded) {
+                searchedList = state.searchedList;
+              } else if (state is HomeTranstion) {
+                searchedList = [];
+              }
+            },
+            builder: (context, state) => state is SearchedTaskLoading
+                ? Lottie.asset(JsonAssets.search)
+                : searchedList.isNotEmpty
+                    ? CustomTaskList(
+                        taskList: searchedList,
+                      )
+                    : Visibility(
+                        visible:
+                            state is SearchedTaskLoaded && searchedList.isEmpty,
+                        child: Lottie.asset(JsonAssets.empty),
+                      ),
           ),
-        ],
-        body: Container(),
+        ),
       ),
     );
   }
