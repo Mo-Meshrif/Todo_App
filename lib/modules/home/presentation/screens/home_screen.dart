aef3620f3b5ddb4e92d544217dc483b4ccc5733a
mo-meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2022-09-17 04:29:30 +0200

    add home screen logic

--- lib/modules/home/presentation/screens/home_screen.dart
+++ lib/modules/home/presentation/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../app/helper/helper_functions.dart';
 import '../../../../app/utils/constants_manager.dart';
 import '../../../../app/utils/routes_manager.dart';
 import '../../../../app/utils/strings_manager.dart';
@@ -8,23 +10,35 @@ import '../../../../app/utils/values_manager.dart';
 import '../components/daily_task.dart';
 import '../components/monthly_task.dart';
 import '../components/weekly_task.dart';
+import '../controller/home_bloc.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/custom_drawer.dart';
 import '../widgets/custom_tab_header.dart';
 import '../widgets/custom_text_search.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  @override
+  void initState() {
+    BlocProvider.of<HomeBloc>(context).add(GetDailyTasksEvent());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
       appBar: const CustomAppBar(),
       drawer: const CustomDrawer(),
       body: DefaultTabController(
         length: AppConstants.homeTabLength,
-        child: NestedScrollView(
-          headerSliverBuilder: (_, __) => [
+        child: CustomScrollView(
+          slivers: [
             SliverAppBar(
               automaticallyImplyLeading: false,
               flexibleSpace: CustomTextSearch(
@@ -32,11 +46,20 @@ class HomeScreen extends StatelessWidget {
                   Routes.searchRoute,
                 ),
               ),
+              pinned: true,
             ),
             SliverPersistentHeader(
               delegate: TabHeader(
                 minHeight: AppSize.s110.h,
                 maxHeight: AppSize.s128.h,
+                onTap: (val) {
+                  if (index != val) {
+                    setState(() {
+                      index = val;
+                    });
+                    HelperFunctions.getTasksOnTab(context, index);
+                  }
+                },
                 tabs: [
                   Tab(text: AppStrings.daily.tr()),
                   Tab(text: AppStrings.weekly.tr()),
@@ -44,18 +67,22 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
               pinned: true,
+            ),
+            SliverFillRemaining(
+              child: Container(
+                color: Colors.white,
+                child: const TabBarView(
+                  physics: NeverScrollableScrollPhysics(),
+                  children: [
+                    DailyTask(),
+                    WeeklyTask(),
+                    MonthlyTask(),
+                  ],
+                ),
+              ),
             )
           ],
-          body: const TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: [
-              DailyTask(),
-              WeeklyTask(),
-              MonthlyTask(),
-            ],
-          ),
         ),
       ),
     );
-  }
 }
